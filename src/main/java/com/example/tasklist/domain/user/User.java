43528d59b12cac14e6eab86662bba78f6fdd0316
alex <alex0072223@mail.ru>
2024-10-14 10:38:50 +0400

    1

---
+++ src/main/java/com/example/tasklist/domain/user/User.java
@@ -0,0 +1,20 @@
+package com.example.tasklist.domain.user;
+
+import lombok.Data;
+import org.springframework.scheduling.config.Task;
+
+import java.util.List;
+import java.util.Set;
+
+@Data //заменяет собой гетеры сетеры и toString()
+public class User {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    private String passwordConfirmation;
+    private Set<Role> roles;
+    private List<Task> tasks;
+
+}
